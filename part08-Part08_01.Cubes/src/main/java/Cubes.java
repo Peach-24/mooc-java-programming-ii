2134d666f89ce176992c9b1a732367d8472960af
Josh Peachey <joshpeachey24@gmail.com>
2023-01-04 20:25:44 +0000

    first commit

---
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            String valueEntered = scanner.nextLine();
+            if (valueEntered.equals("end")) {
+                System.out.println("end");
+                break;
+            } else {
+                int intEntered = Integer.valueOf(valueEntered);
+                int cubedValue = intEntered * intEntered * intEntered;
+                System.out.println(cubedValue);
+            }
+        }
+    }
+}
